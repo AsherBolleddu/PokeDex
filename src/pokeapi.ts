904804140a3fba1579d3c925d224cb769504098f
Asher Bolleddu <104097139+AsherBolleddu@users.noreply.github.com>
2025-11-02 21:08:28 -0600

    Working on pokeAPI

---
+++ src/pokeapi.ts
@@ -0,0 +1,25 @@
+export class PokeAPI {
+  private static readonly baseURL = "https://pokeapi.co/api/v2";
+
+  constructor() {}
+
+  async fetchLocations(pageURL?: string): Promise<ShallowLocations> {
+    // implement this
+  }
+
+  async fetchLocation(locationName: string): Promise<Location> {
+    // implement this
+  }
+}
+
+export type ShallowLocations = {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: {
+    name: string;
+    url: string;
+  }[];
+};
+
+export type Location = {};
